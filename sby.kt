8d7f0252adfa79c27c6f9fa5dbbe3a8d93c1a113
dinulislami <55618712+dinulislami@users.noreply.github.com>
2019-10-11 18:49:10 +0700

    Create sby.kt

---
+++ sby.kt
@@ -0,0 +1,12 @@
+package com.example.listviewicon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sby : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sby)
+    }
+}
